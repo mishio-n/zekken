8bcfffe8e2251543f267165c16a50d76e98a7040
mishio-n <doncool340@gmail.com>
2023-05-22 23:36:06 +0900

    add type:module

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  console.log(req);
   res.status(200).json({
     message: "Hello world",
     cookies: req.cookies,
